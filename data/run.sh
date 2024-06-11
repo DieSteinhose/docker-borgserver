fded7515e4e2e37d506e3955428b03059cd082f1
DieSteinhose <39858483+DieSteinhose@users.noreply.github.com>
2024-06-11 20:06:56 +0200

    added support for folders in /sshkeys/clients and /backup

--- data/run.sh
+++ data/run.sh
@@ -9,7 +9,6 @@ groupmod -o -g "$PGID" borg &>/dev/null
 
 BORG_DATA_DIR=/backup
 SSH_KEY_DIR=/sshkeys
-BORG_CMD='cd ${BORG_DATA_DIR}/${client_name}; borg serve --restrict-to-path ${BORG_DATA_DIR}/${client_name} ${BORG_SERVE_ARGS}'
 AUTHORIZED_KEYS_PATH=/home/borg/.ssh/authorized_keys
 
 # Append only mode?
@@ -62,18 +61,29 @@ echo " * Starting SSH-Key import..."
 # Add every key to borg-users authorized_keys
 rm ${AUTHORIZED_KEYS_PATH} &>/dev/null
 for keyfile in $(find "${SSH_KEY_DIR}/clients" ! -regex '.*/\..*' -a -type f); do
+    # Determine client_name and client_dir
+    relative_path=$(realpath --relative-to="${SSH_KEY_DIR}/clients" "${keyfile}")
     client_name=$(basename ${keyfile})
-    mkdir ${BORG_DATA_DIR}/${client_name} 2>/dev/null
-    echo "  ** Adding client ${client_name} with repo path ${BORG_DATA_DIR}/${client_name}"
+    client_dir=$(dirname ${relative_path})
 
-	# If client is $BORG_ADMIN unset $client_name, so path restriction equals $BORG_DATA_DIR
+    # Create corresponding directories in BORG_DATA_DIR
+    mkdir -p ${BORG_DATA_DIR}/${client_dir}/${client_name} 2>/dev/null
+    echo "  ** Adding client ${client_name} with repo path ${BORG_DATA_DIR}/${client_dir}/${client_name}"
+
+    # If client is $BORG_ADMIN unset $client_name and client_dir, so path restriction equals $BORG_DATA_DIR
 	# Otherwise add --append-only, if enabled
-	borg_cmd=${BORG_CMD}
-	if [ "${client_name}" == "${BORG_ADMIN}" ] ; then
+    if [ "${client_name}" == "${BORG_ADMIN}" ]; then
 		echo "   ** Client '${client_name}' is BORG_ADMIN! **"
-		unset client_name
-	elif [ "${BORG_APPEND_ONLY}" == "yes" ] ; then
-		borg_cmd="${BORG_CMD} --append-only"
+        borg_cmd="cd ${BORG_DATA_DIR}; borg serve --restrict-to-path ${BORG_DATA_DIR} ${BORG_SERVE_ARGS}"
+    else
+        if [ "${client_dir}" == "." ]; then
+            borg_cmd="cd ${BORG_DATA_DIR}/${client_name}; borg serve --restrict-to-path ${BORG_DATA_DIR}/${client_name} ${BORG_SERVE_ARGS}"
+        else
+            borg_cmd="cd ${BORG_DATA_DIR}/${client_dir}/${client_name}; borg serve --restrict-to-path ${BORG_DATA_DIR}/${client_dir}/${client_name} ${BORG_SERVE_ARGS}"
+        fi
+        if [ "${BORG_APPEND_ONLY}" == "yes" ]; then
+            borg_cmd="${borg_cmd} --append-only"
+        fi
 	fi
 
   echo -n "restrict,command=\"$(eval echo -n \"${borg_cmd}\")\" " >> ${AUTHORIZED_KEYS_PATH}
